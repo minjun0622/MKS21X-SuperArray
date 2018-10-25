312e3315612c8275d9345e4a9bb8cde0c865c29d
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-10-25 01:22:42 -0400

    Progress made.

---
+++ SuperArray.java
@@ -0,0 +1,26 @@
+public class SuperArray {
+  private String[] data;
+  private int size;
+
+  public class
+
+  public void clear() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return (size = 0);
+  }
+  public boolean add(String) {
+
+  }
+  public String toString() {
+    return "[]"
+  }
+  public String toStringDebug() {
+
+  }
+  public String get(int) {
+    
+  }
+}
